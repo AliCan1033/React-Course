f27b4f348a76d1c798b62e4000d2db57c1f5a49a
Alican Çatalbaş <turk.can.1071@gmail.com>
2023-02-09 15:22:58 +0300

    Conditional Templates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AppHeader from './AppHeader';
 import { useState, useEffect } from 'react';
 import Bloglist from './Bloglist';
+import Company from './Company';
 
 function App() {
   const couse = "React js"
@@ -13,25 +14,35 @@ function App() {
     { 'id': '4', 'title': 'jQuery', 'desc': 'this content described about jQuery', 'author': 'rishan' },
     { 'id': '5', 'title': 'sql', 'desc': 'this content described about Sql', 'author': 'rishan' }
   ]);
-  
-  const [header,headchange]=useState("React class")
+
+  const [header, headchange] = useState("React class");
+  const [compantdate, campanychange] = useState(null);
+  const [isloaded, changeload] = useState(true);
+
   const FunctionDelete = (id) => {
-    // //console.log(id);  
-    // const newlist = blogdate.filter(item => item.id != id);
-    // change(newlist)
+
     headchange("React");
     console.log(header);
   }
   useEffect(() => {
-    console.log("use effect")
-  }, [blogdate,header])
+
+    fetch("http://localhost:8000/company").then(res => {
+      return res.json();
+
+    }).then(result => {
+           //setTimeout(() => {
+           campanychange(result);
+           changeload(false);
+      // }, 3000);
+
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <AppHeader title="Welcome to react" course={couse}></AppHeader>
-        {/* <Bloglist blog={blogdate.filter(item=>item.author=="rishan")} author="Author is rishan"/>
-        <Bloglist blog={blogdate.filter(item=>item.author=="jhon")} author="Author is jhon"/> */}
-        <Bloglist blog={blogdate} author="All blogs" FunctionDelete={FunctionDelete} />
+        {isloaded && <div>Please Wait .....</div>}
+        {compantdate && <Company companydata={compantdate}></Company>}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
